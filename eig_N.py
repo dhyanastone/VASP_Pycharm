1f6b546d7e4bd07cd22f3af33b5080fa2188f6e3
yaoqiushi <yaoqiushistone@163.com>
2018-09-11 10:49:10 +0800

    Modify 'eig_N.py'

--- eig_N.py
+++ eig_N.py
@@ -22,12 +22,12 @@ def get_nelect(outcar):
     return f_nelect
 
 
-def get_nkpoints(outcar):
-    """Return NKPOINTS read from OUTCAR"""
-    os.system("grep 'irreducible' %s | awk '{print $2}' > .nkpoints" % outcar)
-    with open('.nkpoints', 'r') as nkpoints_file:
-        f_nkpoints = int(nkpoints_file.readline().split()[0])
-    return f_nkpoints
+def get_nkpts_nbands(outcar):
+    """Return NKPTS and NBANDS read from OUTCAR"""
+    os.system("grep 'NKPTS' %s | awk '{print $4,$15}' > .nkpts_nbands" % outcar)
+    with open('.nkpts_nbands', 'r') as nkpts_nbands_file:
+        f_nkpts, f_nbands = map(int, nkpts_nbands_file.readline().split())
+    return f_nkpts, f_nbands
 
 
 def get_kpoints_weight(outcar, f_nkpoints):
@@ -46,17 +46,25 @@ def get_kpoints_weight(outcar, f_nkpoints):
     return f_weight_sum, f_weight
 
 
-def get_eig(outcar, f_nkpoints, f_spin, f_band_no):
+def get_eig(outcar, f_ispin, f_nkpts, f_nbands, f_spin, f_band_no):
     """Return eigenvalue of kpoints"""
-    if f_spin == 1:
-        os.system("grep '  %s  ' %s | head -n %s | awk '{print $2}' > .eig"
-                  % (str(f_band_no), outcar, str(f_nkpoints)))
+    os.system("grep -A %s 'E-fermi' %s > .eig_file"
+              % (str(f_ispin * f_nkpts * (f_nbands + 3) + 6), outcar))
+    if f_ispin == 1:
+        # To write#################
+        pass
+        ###########################
     else:
-        os.system("grep '  %s  ' %s | tail -n %s | awk '{print $2}' > .eig"
-                  % (str(f_band_no), outcar, str(f_nkpoints)))
+        if f_spin == 1:
+            # do not use 'head' to pick, use '-m', search 'grep: write error: Broken pipe' (Evernote)
+            os.system("grep '^[ ]*%s  ' .eig_file -m %s | awk '{print $2}' > .eig"
+                      % (str(f_band_no), str(f_nkpts)))
+        else:
+            os.system("grep '^[ ]*%s  ' .eig_file | tail -n %s | awk '{print $2}' > .eig"
+                      % (str(f_band_no), str(f_nkpts)))
     f_eig = []
     with open('.eig', 'r') as eig_file:
-        for f_j in range(f_nkpoints):
+        for f_j in range(f_nkpts):
             f_eig.append(eig_file.readline().split()[0])
     return f_eig
 
@@ -64,14 +72,15 @@ def get_eig(outcar, f_nkpoints, f_spin, f_band_no):
 print '\nThis program can get eigenvalue(N), see Phys. Rev. B 80, 085202 (2009), eq(2) for details.\n'
 ispin = get_ispin('OUTCAR')
 nelect = get_nelect('OUTCAR')
-nkpoints = get_nkpoints('OUTCAR')
-weight_sum, weight = get_kpoints_weight('OUTCAR', nkpoints)
-print 'ISPIN = %d, NELECT = %f\n' % (ispin, nelect)
+nkpts, nbands = get_nkpts_nbands('OUTCAR')
+weight_sum, weight = get_kpoints_weight('OUTCAR', nkpts)
+print 'ISPIN = %d, NELECT = %f, NKPTS = %d, NBANDS = %d\n' % (ispin, nelect, nkpts, nbands)
 band_no = int(raw_input('Please input band NO.(Hint: NELECT/2):\n'))
 spin = int(raw_input('Please input spin component (Hint 1 or 2):\n'))
-eig = get_eig('OUTCAR', nkpoints, spin, band_no)
-eigenvalue = 0
-for i in range(nkpoints):
+eig = get_eig('OUTCAR', ispin, nkpts, nbands, spin, band_no)
+eigenvalue = 0.0
+for i in range(nkpts):
     eigenvalue = eigenvalue + float(eig[i]) * float(weight[i])
 eigenvalue_norm = eigenvalue / float(weight_sum)
 print 'Eigenvalue of band %d spin component %d is %f eV.\n' % (band_no, spin, eigenvalue_norm)
+os.system("rm -f .eig .eig_file .ispin .nelect .nkpts_nbands .weight .weight_sum")
