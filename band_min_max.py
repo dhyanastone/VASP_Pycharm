6a4b4cb4811316aa18922abbc4b1a7f90ce2caf2
yaoqiushi <yaoqiushistone@163.com>
2018-09-11 12:00:12 +0800

    modify eig_N.py, add band_min_max.py

--- band_min_max.py
+++ band_min_max.py
@@ -3,7 +3,7 @@
 
 import os
 
-"""This program can get eigenvalue(N), see Phys. Rev. B 80, 085202 (2009), eq(2) for details."""
+"""This program can get minimum and maximum eigenvalues of band N spin component M."""
 
 
 def get_ispin(outcar):
@@ -30,22 +30,6 @@ def get_nkpts_nbands(outcar):
     return f_nkpts, f_nbands
 
 
-def get_kpoints_weight(outcar, f_nkpoints):
-    """Return KPOINTS weight and weight sum read from OUTCAR"""
-    os.system("grep -A %s 'irreducible' %s | tail -n %s | awk '{print $4}' > .weight"
-              % (str(f_nkpoints + 3), outcar, str(f_nkpoints)))
-    # list "weight" save weights of kpoints
-    f_weight = []
-    with open('.weight', 'r') as weight_file:
-        for f_i in range(f_nkpoints):
-            f_weight.append(weight_file.readline().split()[0])
-    os.system("grep -A %s 'irreducible' %s | tail -n %s | awk '{print $4}'| paste -sd + | bc > .weight_sum"
-              % (str(f_nkpoints + 3), outcar, str(f_nkpoints)))
-    with open('.weight_sum', 'r') as weight_sum_file:
-        f_weight_sum = weight_sum_file.readline().split()[0]
-    return f_weight_sum, f_weight
-
-
 def get_eig(outcar, f_ispin, f_nkpts, f_nbands, f_spin, f_band_no):
     """Return eigenvalue of kpoints"""
     os.system("grep -A %s 'E-fermi' %s > .eig_file"
@@ -65,22 +49,17 @@ def get_eig(outcar, f_ispin, f_nkpts, f_nbands, f_spin, f_band_no):
     f_eig = []
     with open('.eig', 'r') as eig_file:
         for f_j in range(f_nkpts):
-            f_eig.append(eig_file.readline().split()[0])
+            f_eig.append(float(eig_file.readline().split()[0]))
     return f_eig
 
 
-print '\nThis program can get eigenvalue(N), see Phys. Rev. B 80, 085202 (2009), eq(2) for details.\n'
+print "\nThis program can get minimum and maximum eigenvalues of specified band and spin component.\n"
 ispin = get_ispin('OUTCAR')
 nelect = get_nelect('OUTCAR')
 nkpts, nbands = get_nkpts_nbands('OUTCAR')
-weight_sum, weight = get_kpoints_weight('OUTCAR', nkpts)
 print 'ISPIN = %d, NELECT = %f, NKPTS = %d, NBANDS = %d\n' % (ispin, nelect, nkpts, nbands)
 band_no = int(raw_input('Please input band NO.(Hint: NELECT/2):\n'))
 spin = int(raw_input('\nPlease input spin component (Hint: 1 or 2):\n'))
 eig = get_eig('OUTCAR', ispin, nkpts, nbands, spin, band_no)
-eigenvalue = 0.0
-for i in range(nkpts):
-    eigenvalue = eigenvalue + float(eig[i]) * float(weight[i])
-eigenvalue_norm = eigenvalue / float(weight_sum)
-print '\nEigenvalue of band %d spin component %d is %f eV.\n' % (band_no, spin, eigenvalue_norm)
-os.system("rm -f .eig .eig_file .ispin .nelect .nkpts_nbands .weight .weight_sum")
+print '\n(minimum, maximum) of band %d spin component %d is (%f, %f) eV.\n' % (band_no, spin, min(eig), max(eig))
+os.system("rm -f .eig .eig_file .ispin .nelect .nkpts_nbands")
